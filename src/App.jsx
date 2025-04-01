1198dbfde5acc8b1add0af373d050dd6a455564c
Yacoubbeltanivelle <yacoubbeltanivelle@gmail.com>
2025-04-01 12:01:16 +0200

    ACTIVITE 5 : Créer la requête permettant de modifier complètement un produit

--- src/App.jsx
+++ src/App.jsx
@@ -37,37 +37,49 @@ function App() {
     console.log("Produit créé :", data);
   }
 
+  // Ajouter un produit via POST
+  async function putProducts(id) {
+    const response = await fetch(`https://fakestoreapi.com/products/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: "Produit mis à jour",
+        price: 49.99,
+        description: "Description mise à jour",
+        image: "https://via.placeholder.com/150",
+        category: "electronics",
+      }),
+    });
+
+    const data = await response.json();
+    alert(`Le produit avec l'id ${data.id} a été modifié`);
+    console.log("Produit modifié :", data);
+  }
+
   return (
     <Container>
-      <Row className="mb-4">
-        <Col>
-          <Button onClick={postProducts}>Ajouter un produit</Button>
-        </Col>
-      </Row>
+      <Col>
+        <Button onClick={postProducts}>Ajouter un produit</Button>
+      </Col>
 
-      <Row>
+      <Row className="gy-4">
         {products.map((product) => (
-          <Col key={product.id} className="mb-4 mx-2">
-            <Card style={{ width: "18rem", overflow: "hidden" }}>
-              <Card.Img
-                variant="top"
-                src={product.image}
-                alt={product.title}
-                style={{
-                  height: "200px",
-                  objectFit: "contain",
-                  padding: "1rem",
-                }}
-              />
+          <Col md={3} lg={3} key={product.id}>
+            <Card className="card h-100">
+              <Card.Img variant="top" src={product.image} alt={product.title} />
               <Card.Body>
-                <Card.Title style={{ height: "100px", overflow: "hidden" }}>
-                  {product.title}
-                </Card.Title>
-                <Card.Text style={{ height: "150px", overflow: "auto" }}>
-                  {product.description}
-                </Card.Text>
-                <Card.Text>Prix : {product.price} €</Card.Text>
+                <Card.Title>{product.title}</Card.Title>
+                <Card.Text className="text">{product.description}</Card.Text>
+                <Card.Text className="text">Prix : {product.price} €</Card.Text>
                 <Button onClick={postProducts}>
+                  Modifier le produit complet
+                </Button>
+                <Button
+                  variant="warning"
+                  onClick={() => putProducts(product.id)}
+                >
                   Modifier le produit complet
                 </Button>
               </Card.Body>
